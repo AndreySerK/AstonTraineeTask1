52b851e921da984e955b889b54df23d9b0d7d7e9
AndreySerK <omk.ask@gmail.com>
2024-03-23 14:00:50 +0100

    add JavaDoc and Tests

--- src/main/java/org/example/CustomArrayList.java
+++ src/main/java/org/example/CustomArrayList.java
@@ -1,116 +1,207 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
-public class CustomArrayList<E>  {
-    private int size = 0;
-    private static final int DEFAULT_CAPACITY = 10;
-    private static final Object[] EMPTY_ELEMENTS = {};
-    private Object[] elements;
+/**
+ * Кастомная имплементация ArrayList
+ *
+ * @param <E> тип элементов листа
+ */
+public class CustomArrayList<E> {
 
+    // количество заполненных элементов, видимый пользователю размер контейнера
+    private int size;
+
+    // дефолтный размер массива
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // пустой массив
+    private static final Object[] EMPTY_ELEMENTS = {};
+
+    // массив, в котором будут храниться элементы
+    private Object[] data;
+
+    /**
+     * Конструктор без параметров (создает объект с дефолтным размером массива)
+     */
     public CustomArrayList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        this.data = new Object[DEFAULT_CAPACITY];
     }
 
+    /**
+     * Конструктор с параметром, задающим размер массива
+     */
     public CustomArrayList(int initialCapacity) {
         if (initialCapacity > 0) {
-            this.elements = new Object[initialCapacity];
+            this.data = new Object[initialCapacity];
         } else if (initialCapacity == 0) {
-            this.elements = EMPTY_ELEMENTS;
+            this.data = EMPTY_ELEMENTS;
         } else {
             throw new IllegalArgumentException("Illegal Capacity: " +
                     initialCapacity);
         }
     }
 
+    /**
+     * Возвращает количество элементов массива
+     */
+    public int size() {
+        return size;
+    }
 
-
+    /**
+     * Добавляет элемент в конец списка
+     *
+     * @param e элемент для добавления в конец списка
+     * @return true если элемент добавлен успешно
+     */
     public boolean add(E e) {
         add(size, e);
         return true;
     }
 
-
+    /**
+     * Вставляет элемент на заданную позицию списка.
+     * Сдвигает элемент или последовательность элементов (если такие есть) с позиции на которую была вставка вправо.
+     *
+     * @param index   индекс, на который нужно вставить элемент
+     * @param element элемент для вставки
+     * @throws IndexOutOfBoundsException если индекс не в границах массива
+     *                                   (метод checkIndex (index < 0 || index > size))
+     */
     public void add(int index, E element) {
         checkIndex(index, size);
-        ensureCapacity();
-        for (int i = size - 1; i >= index; i--) {
-            elements[i + 1] = elements[i];
+        if (size() == data.length)
+            increaseCapacity();
+        for (int i = size() - 1; i >= index; i--) {
+            data[i + 1] = data[i];
         }
-        elements[index] = element;
+        data[index] = element;
         size++;
     }
 
-
+    /**
+     * Возвращает элемент по заданному индексу
+     *
+     * @param index индекс элемента
+     * @throws IndexOutOfBoundsException если индекс не в границах массива
+     *                                   (метод checkIndex (index < 0 || index > size))
+     */
     public E get(int index) {
         checkIndex(index, size);
-        return (E) elements[index];
+        return (E) data[index];
     }
 
+    /**
+     * Заменяет элемент по заданному индексу, возвращает старый элемент
+     *
+     * @param index индекс элемента
+     * @throws IndexOutOfBoundsException если индекс не в границах массива
+     *                                   (метод checkIndex (index < 0 || index > size))
+     */
     public E set(int index, E element) {
         checkIndex(index, size);
-        E oldValue = (E) elements[index];
-        elements[index] = element;
+        E oldValue = (E) data[index];
+        data[index] = element;
         return oldValue;
     }
 
 
+    /**
+     * Удаляет элемент по заданному индексу, возвращает удаленный элемент
+     *
+     * @param index индекс элемента
+     * @throws IndexOutOfBoundsException если индекс не в границах массива
+     *                                   (метод checkIndex (index < 0 || index >= size))
+     */
     public E remove(int index) {
         checkIndex(index, size);
-        Object element = elements[index];
+        Object element = data[index];
         for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
+            data[i] = data[i + 1];
         }
         size--;
         return (E) element;
     }
 
 
+    /**
+     * Очищает массив от элементов
+     */
     public void clear() {
-        final Object[] es = elements;
+        final Object[] es = data;
         for (int to = size, i = size = 0; i < to; i++)
             es[i] = null;
     }
 
+    /**
+     * Проверяет индекс на попадание в границы массива
+     *
+     * @param index заданный индекс
+     * @param size  размер массива
+     */
     private void checkIndex(int index, int size) {
-        if (index >= size || index < 0) {
+        if (index > size || index < 0) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
         }
     }
 
-    private void ensureCapacity() {
-        int newSize = elements.length * 2;
-        elements = Arrays.copyOf(elements, newSize);
+    /**
+     * Увеличивает вместимость массива в два раза
+     */
+    private void increaseCapacity() {
+        // нужно создать массив в 2 раза больше
+        Object[] newData = new Object[data.length * 2];
+        // скопировать элементы от 0 до data.length из старого массива в новый
+        System.arraycopy(data, 0, newData, 0, data.length);
+        // присвоить data ссылку на новый массив
+        data = newData;
     }
 
-    public void quickSort (E [] array, int low, int high, Comparator<E> comparator){
-        if (array.length == 0)
-            return;//завершить выполнение, если длина массива равна 0
+    /**
+     * Вспомогательный метод - вызывает метод быстрой сортировки {@link CustomArrayList#quickSort(Object[], int, int, Comparator)}
+     *
+     * @param comparator объект компаратора для установления порядка сортировки
+     */
+    public void sort(Comparator comparator) {
+        quickSort((E[]) data, 0, size - 1, comparator);
+    }
+
+    /**
+     * Реализация быстрой сортировки массива
+     *
+     * @param data       массив для сортировки
+     * @param low        начальный индекс массива
+     * @param high       конечный индекс массива
+     * @param comparator объект компаратора для установления порядка сортировки
+     */
+    private void quickSort(E[] data, int low, int high, Comparator comparator) {
+        if (data.length == 0)
+            return; //завершить выполнение, если длина массива равна 0
 
         if (low >= high)
-            return;//завершить выполнение если уже нечего делить
+            return; //завершить выполнение если уже нечего делить
 
         // выбрать опорный элемент
-        int middle = (int) (low + (high - low)*Math.random());
-        E pivot = array[middle];
+        int middle = (int) (low + (high - low) * Math.random());
+        E pivot = data[middle];
 
-        // разделить на подмассивы больше и меньше опорного элемента
+        // разделить на подмассивы больше и меньше опорного
         int i = low, j = high;
         while (i <= j) {
-            while (comparator.compare(array[i], pivot) < 0) {
+            while (comparator.compare(data[i], pivot) < 0) {
                 i++;
             }
 
-            while (comparator.compare(array[j], pivot) > 0) {
+            while (comparator.compare(data[j], pivot) > 0) {
                 j--;
             }
 
-            //меняем местами
+            //если элемент low больше опорного, а high меньше - меняем местами
             if (i <= j) {
-                E temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                E temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
                 i++;
                 j--;
             }
@@ -118,13 +209,21 @@ public class CustomArrayList<E>  {
 
         // вызов рекурсии для сортировки левой и правой части
         if (low < j)
-            quickSort(array, low, j, comparator);
+            quickSort(data, low, j, comparator);
 
         if (high > i)
-            quickSort(array, i, high, comparator);
+            quickSort(data, i, high, comparator);
     }
 
-    public static void main(String[] args) {
-
+    @Override
+    public String toString() {
+        StringBuilder r = new StringBuilder("[");
+        for (int i = 0; i < size(); i++) {
+            if (i != 0)
+                r.append(", ");
+            r.append(data[i]);
+        }
+        r.append("]");
+        return r.toString();
     }
 }
---
+++ src/test/java/org/example/CustomArrayListTest.java
@@ -0,0 +1,129 @@
+package org.example;
+
+
+import com.sun.jdi.IntegerValue;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.*;
+
+
+public class CustomArrayListTest {
+
+    CustomArrayList<Object> customArrayList;
+
+    @Before
+    public void setUp() throws Exception {
+        customArrayList = new CustomArrayList<>();
+    }
+
+    @Test
+    public void getSizeTest() {
+        customArrayList.add(5);
+        assertEquals(1, customArrayList.size());
+    }
+
+    @Test
+    public void whenGetWithCorrectIndex_thenReturnElement_getTest() {
+        customArrayList.add(5);
+        customArrayList.add(6);
+        assertEquals(5, customArrayList.get(0));
+    }
+
+    @Test
+    public void whenGetWithWrongIndex_thenThrowException_getTest() {
+        customArrayList.add(5);
+        customArrayList.add(6);
+        Throwable thrown = assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.get(10));
+        assertNotNull(thrown.getMessage());
+    }
+
+    @Test
+    public void whenAddWithoutIndex_thenReturnTrue_addTest() {
+        assertTrue(customArrayList.add(5));
+        assertEquals(1, customArrayList.size());
+        assertEquals(5, customArrayList.get(0));
+    }
+
+    @Test
+    public void whenAddMoreThan20_thenReturnTrue_addTest() {
+        for (int i = 0; i < 25; i++) {
+            customArrayList.add(i);
+        }
+        assertEquals(25, customArrayList.size());
+    }
+
+    @Test
+    public void whenAddWithCorrectIndex_thenNoException_addTest() {
+        customArrayList.add(0, 1);
+        customArrayList.add(1, 2);
+        customArrayList.add(2, 3);
+        customArrayList.add(2, 8);
+        assertEquals(4, customArrayList.size());
+        assertEquals(8, customArrayList.get(2));
+    }
+
+    @Test
+    public void whenAddWithWrongIndex_thenThrowException_addTest() {
+        Throwable thrown = assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.add(10, 10));
+        assertNotNull(thrown.getMessage());
+    }
+
+
+    @Test
+    public void whenSetWithCorrectIndex_thenReturnOldElement_setTest() {
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.set(1, 10);
+        assertEquals(10, customArrayList.get(1));
+    }
+
+    @Test
+    public void whenSetWithWrongIndex_thenThrowException_setTest() {
+        Throwable thrown = assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.set(10, 10));
+        assertNotNull(thrown.getMessage());
+    }
+
+    @Test
+    public void whenRemoveWithCorrectIndex_thenReturnRemovedElement_removeTest() {
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.remove(0);
+        assertEquals(1, customArrayList.size());
+        assertEquals(2, customArrayList.get(0));
+    }
+
+    @Test
+    public void whenRemoveWithWrongIndex_thenThrowException_removeTest() {
+        customArrayList.add(6);
+        Throwable thrown = assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.remove(10));
+        assertNotNull(thrown.getMessage());
+    }
+
+    @Test
+    public void clearTest() {
+        customArrayList.add(1);
+        customArrayList.add(2);
+        assertEquals(2, customArrayList.size());
+        customArrayList.clear();
+        assertEquals(0, customArrayList.size());
+    }
+
+    @Test
+    public void sortTest() {
+        CustomArrayList<Integer> cList = new CustomArrayList<>();
+        Comparator<Integer> comparator = (o1, o2) -> o1 - o2;
+        customArrayList.add(8);
+        customArrayList.add(5);
+        customArrayList.add(7);
+        customArrayList.add(9);
+        customArrayList.add(0);
+        String givenList = "[8, 5, 7, 9, 0]";
+        String sortedList = "[0, 5, 7, 8, 9]";
+        assertEquals(givenList, customArrayList.toString());
+        customArrayList.sort(comparator);
+        assertEquals(sortedList, customArrayList.toString());
+    }
+}
